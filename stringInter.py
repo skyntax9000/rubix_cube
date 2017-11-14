83e096ba772e7b8168e9a36b37f799a1bf1d9a6b
admin <admin@admins-Air.localdomain>
2017-11-14 13:37:13 -0600

    String Interpolation Assignment, 11/14/17

---
+++ stringInter.py
@@ -0,0 +1,6 @@
+firstName = input("Please enter your first name: ")
+lastName = input("Please enter your last name: ")
+
+message = "Hello, My name is {0} {1}.".format(firstName,lastName)
+
+print(message)
